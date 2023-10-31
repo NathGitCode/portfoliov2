b36cd08a1d5f133f27f59978656c75633ca38308
Nathan Stringer <nathstringer@gmail.com>
2023-10-31 17:30:02 +0000

    info started, end of day 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import React from "react";
 import "./App.css";
+import Projects from "./comps/projects/Projects";
+import Info from "./comps/info/Info";
+import NavBar from "./comps/navbar/NavBar";
+import About from "./comps/about/About";
 
 function App() {
   return (
     <div className="App">
-      <div></div>
+      <NavBar />
+      <Info />
+      <About />
+      <Projects />
     </div>
   );
 }
